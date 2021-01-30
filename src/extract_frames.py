df30bb0f9aa6e0d9d162cb474549433f3bb01814
Javi Naranjo-Alcazar <javi3naranjo@gmail.com>
2021-01-30 12:44:12 +0100

    updating extract frames with filename and extension

--- src/extract_frames.py
+++ src/extract_frames.py
@@ -2,17 +2,18 @@ import cv2
 import os
 
 
-def get_images_from_video(path2video, path2store, second=1):
+def get_images_from_video(path2video, path2store, second=1, extension='.jpg'):
     """
-
+    Function that slides a video and saves image according to seconds period
     Args:
+
         path2video (str): video path to be splitted
         path2store (str): path to store the image file
         second (int): value indicating every how many seconds an image is created
+        extension (str): image format to be stored
     """
 
-    # TODO: filename that can be mapped to the video, extension of the image (jpg or png)
-
+    filename = path2video.split('/')[-1][:-4]  # getting video name without extension
     vidcap = cv2.VideoCapture(path2video)
     count = 0
     success = True
@@ -22,13 +23,12 @@ def get_images_from_video(path2video, path2store, second=1):
         success, image = vidcap.read()
         # print('read a new frame:', success)
         if count % (second * fps) == 0:
-            cv2.imwrite(path2store + 'frame%d.jpg' % count, image)
+            cv2.imwrite(path2store + filename + '_frame{}'.format(count) + extension, image)
         #    print('successfully written 10th frame')
         count += 1
 
 
 if __name__ == '__main__':
-
     home = os.getenv('HOME')
 
     get_images_from_video(home + '/repos/DCASE2021-Task1b/dummy_wav/toxicidad.mp4',

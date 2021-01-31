1ed0f4f316d346165438f9b9467de4a804ab08b6
Javi Naranjo-Alcazar <javi3naranjo@gmail.com>
2021-01-31 12:29:38 +0100

    adding config doc and audio network

--- src/config.py
+++ src/config.py
@@ -1,4 +1,8 @@
+"""
+Configuration file: commented in list form are indicated the supported options
+"""
+
 path2image = ''
-pre_trained_network = 'xception'
-n_classes = 10
+pre_trained_network = 'xception'  # ['xception', 'inception_resnet_v2', 'inception_v3']
+n_classes = 10  # [10]
 which_train = 'image'

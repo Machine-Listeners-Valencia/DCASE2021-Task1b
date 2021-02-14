a087723e436595132209241603d95b28e1f1e1f0
Javi Naranjo-Alcazar <javi3naranjo@gmail.com>
2021-02-14 14:01:25 +0100

    exploring mit67 finished

--- src/transfer_learning/explore_datasets.py
+++ src/transfer_learning/explore_datasets.py
@@ -1,14 +1,32 @@
-from ..utils import create_folder, obtain_lines_txt
 import os
 from shutil import copyfile
 from tqdm import tqdm
 
 
+def create_folder(path2folder):
+    if os.path.isdir(path2folder) is False:
+        os.mkdir(path2folder)
+
+
+def obtain_lines_txt(path2file):
+    with open(path2file) as f:
+        lines = [x.strip() for x in f.readlines()]
+        # lines = f.readlines()
+
+    return lines
+
+
 def split_mit67_train_test(path2store, path2trainfile, path2testfile):
+    """
+
+    Args:
+        path2store (str):
+        path2trainfile (str):
+        path2testfile (str):
+    """
     path2split = os.path.join(path2store, 'split')
     create_folder(path2split)
 
-    # TODO: create folders from training and testing and copy files
     trainfiles = obtain_lines_txt(path2trainfile)
     testfiles = obtain_lines_txt(path2testfile)
 
@@ -37,10 +55,17 @@ def split_mit67_train_test(path2store, path2trainfile, path2testfile):
     for j in tqdm(range(0, len(trainfiles))):
         copyfile(os.path.join(path2images, trainfiles[j]), os.path.join(path2train_split, trainfiles[j]))
 
-    print('Copy testing files to corresponding label folder...')
     # Copy test files
+    print('Copy testing files to corresponding label folder...')
     for jj in tqdm(range(0, len(testfiles))):
         copyfile(os.path.join(path2images, testfiles[jj]), os.path.join(path2test_split, testfiles[jj]))
 
 
+if __name__ == '__main__':
+    home = os.getenv('HOME')
 
+    path2store = os.path.join(home, 'repos/DCASE2021-Task1b/data/mit67/indoorCVPR_09')
+    path2trainfile = os.path.join(home, 'repos/DCASE2021-Task1b/data/mit67/TrainImages.txt')
+    path2testfile = os.path.join(home, 'repos/DCASE2021-Task1b/data/mit67/TestImages.txt')
+
+    split_mit67_train_test(path2store, path2trainfile, path2testfile)
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,4 @@
 import os
 
 
-def create_folder(path2folder):
-    if os.path.isdir(path2folder) is False:
-        os.mkdir(path2folder)
 
-
-def obtain_lines_txt(path2file):
-    with open(path2file) as f:
-        lines = f.readlines()
-
-    return lines

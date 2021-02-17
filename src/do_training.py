5df61379276d8497919e390f2189b000a4c4bf2f
Javi Naranjo-Alcazar <javi3naranjo@gmail.com>
2021-02-17 19:12:08 +0100

    adding logger to do_training

--- src/do_training.py
+++ src/do_training.py
@@ -2,40 +2,79 @@ import config
 from training_callbacks import create_callbacks
 from utils import create_training_outputs_folder, save_to_json
 import os
+import logging
 
 
-def train(path2store):
-    path2callbacks = create_training_outputs_folder(path2store)
-
+def train(path2callbacks, logger):
+    logger.info('CREATING CALLBACKS')
     callbacks = create_callbacks(path2callbacks, **config.callbacks_settings)
     save_to_json(os.path.join(path2callbacks, 'callbacks.json'), config.callbacks_settings)
+    logger.info('CALLBACK CONFIGURATION CAN BE CHECKED IN: {}'.format(os.path.join(path2callbacks, 'callbacks.json')))
 
     if config.which_train == 'image':
         from trainers import image_trainer
         from image_networks import construct_keras_image_network
+        logger.info('CONSTRUCTING IMAGE CLASSIFICATION NETWORK')
         model = construct_keras_image_network(include_classification=True,
                                               **config.image_network_settings)
 
         save_to_json(os.path.join(path2callbacks, 'image_network_settings.json'),
                      config.image_network_settings)
+        logger.info(
+            'IMAGE NETWORK CONFIGURATION CAN BE CHECKED IN: {}'.format(os.path.join(path2callbacks,
+                                                                                    'image_network_settings.json')))
         save_to_json(save_to_json(os.path.join(path2callbacks, 'image_generator.json'),
                                   config.image_train_gen_args))
 
+        logger.info(
+            'IMAGE TRAINING CONFIGURATION CAN BE CHECKED IN: {}'.format(os.path.join(path2callbacks,
+                                                                                     'image_generator.json')))
+
+        logger.info('FITTING IMAGE NETWORK ARCHITECTURE: {}'.format(config.image_network_settings['net']))
         image_trainer(model, config.path2image_data, callbacks=callbacks)
 
     if config.which_train == 'audio':
         from trainers import audio_trainer
         from audio_networks import construct_asc_network_csse
+        logger.info('CONSTRUCTING AUDIO CLASSIFICATION NETWORK')
         model = construct_asc_network_csse(include_classification=True,
                                            **config.audio_network_settings)
         save_to_json(os.path.join(path2callbacks, 'audio_network_settings.json'),
                      config.audio_network_settings)
+        logger.info(
+            'AUDIO NETWORK CONFIGURATION CAN BE CHECKED IN: {}'.format(os.path.join(path2callbacks,
+                                                                                    'audio_network_settings.json')))
+
         save_to_json(save_to_json(os.path.join(path2callbacks, 'audio_generator.json'),
                                   config.audio_train_gen_args))
+
+        logger.info(
+            'AUDIO TRAINING CONFIGURATION CAN BE CHECKED IN: {}'.format(os.path.join(path2callbacks,
+                                                                                     'audio_generator.json')))
+
+        logger.info('FITTING AUDIO NETWORK ARCHITECTURE DCASE2020')
         audio_trainer(model, config.path2audio_data, callbacks=callbacks)
 
 
 if __name__ == '__main__':
+    path2callbacks = create_training_outputs_folder(config.path2outputs)
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.NOTSET)
+
+    console_handler = logging.StreamHandler()
+    console_handler_format = '%(asctime)s | %(levelname)s: %(message)s'
+    console_handler.setFormatter(logging.Formatter(console_handler_format))
+    logger.addHandler(console_handler)
+
+    file_handler = logging.FileHandler(os.path.join(path2callbacks, 'logger.log'))
+    file_handler_format = '%(asctime)s | %(levelname)s | %(lineno)d: %(message)s'
+    file_handler.setFormatter(logging.Formatter(file_handler_format))
+    logger.addHandler(file_handler)
+
     home = os.getenv('HOME')
 
-    train(config.path2outputs)
+    logger.info('STARTING TRAINING')
+    logger.info('TRAINING DOMAIN: {}'.format(config.which_train))
+    logger.info('ALL TRAINING INFORMATION WILL BE STORED IN: {}'.format(path2callbacks))
+    train(path2callbacks, logger)
